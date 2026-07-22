f58d71364981bf5bb58b31fa21056e702f055c20
TeluguGFA63 <telugugfa@gmail.com>
2026-07-22 05:14:49 -0300

    Add cart management functions to script.js
    Implement cart functionality with add and remove features.

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-alert("Welcome to Telugu GFA");
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+
+cart.push({
+name: name,
+price: price
+});
+
+localStorage.setItem("cart", JSON.stringify(cart));
+
+alert(name + " added to cart!");
+
+}
+
+function loadCart(){
+
+let cartItems=document.getElementById("cart-items");
+let total=document.getElementById("total");
+
+if(!cartItems) return;
+
+cartItems.innerHTML="";
+
+let grandTotal=0;
+
+cart.forEach((item,index)=>{
+
+grandTotal+=item.price;
+
+cartItems.innerHTML+=`
+<div class="card">
+<h2>${item.name}</h2>
+<h3>₹${item.price}</h3>
+
+<button onclick="removeItem(${index})">
+Remove
+</button>
+
+</div>
+`;
+
+});
+
+total.innerHTML="Total : ₹"+grandTotal;
+
+}
+
+function removeItem(index){
+
+cart.splice(index,1);
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+loadCart();
+
+}
+
+window.onload=loadCart;
