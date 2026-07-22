35e79c7e41521df1d87a13fdacc9896223f80e0f
TeluguGFA63 <telugugfa@gmail.com>
2026-07-22 15:50:43 -0300

    Implement preloader hide on window load
    Add preloader functionality to hide after 2.5 seconds

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+window.addEventListener("load",function(){
+
+setTimeout(function(){
+
+const preloader=document.getElementById("preloader");
+
+preloader.style.opacity="0";
+preloader.style.visibility="hidden";
+
+},2500);
+
+});
